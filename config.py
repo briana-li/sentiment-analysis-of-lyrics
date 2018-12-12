e5782e886b9c2fd738c985389cb89ac13b7e7e72
briana-li <brianali@brandeis.edu>
2018-12-12 16:00:15 -0500

    added new artist

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-artists = ["Ariana Grande", "Drake", "Keith Urban", "Frank Ocean", "Bring Me The Horizon", \
+artists = ["Ariana Grande", "Pharrell Williams", "Keith Urban", "Frank Ocean", "Bring Me The Horizon", \
 "John Mayer", "Eminem", "My Chemical Romance", "Tauren Wells", "Linkin Park"]
