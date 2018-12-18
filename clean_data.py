c89b2b28eaa297070cd8ff1830a5e5e5abf8782b
briana-li <brianali@brandeis.edu>
2018-12-18 15:05:32 -0500

    minor changes

--- clean_data.py
+++ clean_data.py
@@ -12,11 +12,11 @@ for artist in artists:
 
     f.close()
 
-    #remove identifiers like chorus, verse, etc
+    #remove words like chorus, verse, etc
     all_words = re.sub(r'[\(\[].*?[\)\]]', '', all_words)
 
     all_words = os.linesep.join([s for s in all_words.splitlines() if s])
-    print(all_words)
+    #print(all_words)
 
     f = open('lyrics/' + artist + '-cleaned', 'wb')
     f.write(all_words.encode('utf-8'))
